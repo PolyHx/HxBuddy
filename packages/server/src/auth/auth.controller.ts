03cb274ac9406e0e03d469e2f274076007ca96d8
Dominic Hains <contact.dominichains@gmail.com>
2022-07-01 15:30:31 -0400

    feat: me route
    va servire pour l'auth

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { Payload, UserRole } from 'types';
 import { UserService } from 'src/user/user.service';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { ParticipantService } from 'src/participant/participant.service';
 import { CreateParticipantDto } from 'src/participant/dto/create-participant.dto';
+import { JwtAuthGuard } from './jwt.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -13,6 +14,18 @@ export class AuthController {
     private authService: AuthService,
     private participantService: ParticipantService,
   ) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  async me(@Req() req: any) {
+    try {
+      const { user } = req;
+      delete user.password;
+      return user;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   @Post('participant')
   async registerParticipant(
