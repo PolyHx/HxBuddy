53632f94cdb848f75b0df2eceafd76364198935d
Jean-François Senécal <57534854+SenecalJF@users.noreply.github.com>
2022-07-17 14:57:45 -0400

    Revert "Challenges dev"

--- packages/web/src/Router.tsx
+++ packages/web/src/Router.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Navbar from './Navbar';
 import { Register } from './views/Register';
-import ChallengesDashboard from './views/Challenges/ChallengesDashboard';
 
 export const Router = () => {
   return (
@@ -11,7 +10,6 @@ export const Router = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/challenges" element={<ChallengesDashboard />} />
       </Routes>
     </BrowserRouter>
   );
