e307eae43fedac950dfe497597598cd948624159
Dominic Hains <contact.dominichains@gmail.com>
2022-07-20 00:57:57 -0400

    feat: authContext

--- packages/web/src/context/AuthContext.tsx
+++ packages/web/src/context/AuthContext.tsx
@@ -1,93 +1,41 @@
-import axios from 'axios';
-import { createContext, useContext, useEffect, useReducer } from 'react';
+import createDataContext from './createDataContext';
 
-// from: https://theodorusclarence.com/library/auth-context
-
-const StateContext = createContext({
-  authenticated: false,
-  user: null,
-  loading: true,
-});
-
-const DispatchContext = createContext(null);
-
-const reducer = (state: any, { type, payload }: any) => {
-  switch (type) {
-    case 'LOGIN':
+const authReducer = (state: any, action: any) => {
+  switch (action.type) {
+    case 'signout':
+      return { token: null, role: null };
+    case 'signin':
       return {
-        ...state,
-        authenticated: true,
-        user: payload,
-      };
-    case 'LOGOUT':
-      localStorage.removeItem('token');
-      return {
-        ...state,
-        authenticated: false,
-        user: null,
-      };
-    case 'POPULATE':
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          ...payload,
-        },
-      };
-    case 'STOP_LOADING':
-      return {
-        ...state,
-        loading: false,
+        token: action.payload.token,
+        role: action.payload.role,
       };
     default:
-      throw new Error(`Unknown action type: ${type}`);
+      return state;
   }
 };
 
-export const AuthProvider = ({ children }: any) => {
-  const [state, defaultDispatch] = useReducer(reducer, {
-    user: null,
-    authenticated: false,
-    loading: true,
-  });
-
-  const dispatch = (type: any, payload: any) =>
-    defaultDispatch({ type, payload });
-
-  useEffect(() => {
-    const loadUser = async () => {
-      try {
-        const token = localStorage.getItem('token');
-        if (token === null || token === undefined) {
-          return;
-        }
-        const res = await axios.get('/auth/me', {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        dispatch('LOGIN', res.data.data);
-      } catch (err) {
-        console.log(err);
-        localStorage.removeItem('token');
-      } finally {
-        dispatch('STOP_LOADING', {});
-      }
-    };
-
-    loadUser();
-    // eslint-disable-next-line
-  }, []);
-
-  return (
-    <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={dispatch as any}>
-        {children}
-      </DispatchContext.Provider>
-    </StateContext.Provider>
-  );
+const signin = (dispatch: any) => {
+  return ({ token, role }: { token: string; role: string }) => {
+    localStorage.setItem('token', token);
+    dispatch({
+      type: 'signin',
+      payload: {
+        token,
+        role,
+      },
+    });
+  };
 };
 
-export const useAuthState = () => useContext(StateContext);
-export const useAuthDispatch = () => useContext(DispatchContext);
+const signout = (dispatch: any) => {
+  return () => {
+    localStorage.removeItem('token');
+    dispatch({ type: 'signout' });
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signin, signout },
+  { token: null, role: null }
+);
